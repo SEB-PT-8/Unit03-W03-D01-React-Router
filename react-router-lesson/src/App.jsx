4047802b0c7cc73717fca8376bbf0d96876f879e
omarakamal <omaraakamal@gmail.com>
2026-02-15 18:28:35 +0300

    update

--- react-router-lesson/src/App.jsx
+++ react-router-lesson/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import {Routes, Route} from 'react-router'
 function App() {
+
+  // steps when working with react router
+  // 1. install react-router: npm i react-router
+  // 2. Wrap your <App> in BrowserRouter in the main.jsx
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route/>
+      </Routes>
+    </div>
   )
 }
 
