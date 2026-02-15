978bf8570da84613e2aa060018595dc1fc1c2cf3
omarakamal <omaraakamal@gmail.com>
2026-02-15 19:43:45 +0300

    update

--- react-router-lesson/src/pages/AllStudents.jsx
+++ react-router-lesson/src/pages/AllStudents.jsx
@@ -1,5 +1,14 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 function AllStudents() {
+    const [students, setStudents] = useState([])
+    async function getAllStudents(){
+        const allStudents = await axios.get('https://omar-ga-class.onrender.com/students')
+        setStudents(allStudents.data)
+
+
+    }
   return (
     <div>AllStudents</div>
   )
