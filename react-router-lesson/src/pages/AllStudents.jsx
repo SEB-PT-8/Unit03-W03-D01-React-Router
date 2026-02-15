037fc1465b4fc2f6ab7c0d8dc37b565e53f62703
omarakamal <omaraakamal@gmail.com>
2026-02-15 19:50:22 +0300

    update

--- react-router-lesson/src/pages/AllStudents.jsx
+++ react-router-lesson/src/pages/AllStudents.jsx
@@ -6,11 +6,22 @@ function AllStudents() {
     async function getAllStudents(){
         const allStudents = await axios.get('https://omar-ga-class.onrender.com/students')
         setStudents(allStudents.data)
-
-
     }
+
+    useEffect(()=>{
+        getAllStudents()
+    },[])
   return (
-    <div>AllStudents</div>
+    <div>
+        <h1>All Students</h1>
+
+        {students.map((oneStudent)=>
+        <div>
+            <h2>Name: {oneStudent.name}</h2>
+            <p>Course{oneStudent.course}</p>
+        </div>
+        )}
+    </div>
   )
 }
 
